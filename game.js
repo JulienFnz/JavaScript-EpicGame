61f8801afe7a6b5d0afc5e83e66d1ef080d518af
JulienFnz <fernandezj@laposte.net>
2022-03-23 11:55:06 +0100

    test002

--- game.js
+++ game.js
@@ -1,19 +1,26 @@
 
+var audio = new Audio('soundtrack.mp3');
+
+
 var btn = document.querySelector('input');
 var txt = document.querySelector('p');
  
 
 btn.addEventListener('click', updateBtn);
 
+
+
 function updateBtn() {
   if (btn.value === 'Lancez le jeu') {
     btn.value = 'Stop!!!';
     //txt.textContent = "Tu t'es fait Rickroller!";
     txt.innerHTML = "Tu t'es fait Rickroller!" + "<br>" + "<img src=\"loading.gif\"/>";
-    myAudio.src = "soundtrack.mp3"
+    audio.play();
 
   } else {
     btn.value = 'Lancez le jeu';
     txt.textContent = 'Le meilleur jeu en JavaScript !';
   }
 }
+
+
